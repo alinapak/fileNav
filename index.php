6f111c6f5a43d08ca0690fd556bda8435a9f0b4a
alinapak <alina.pakamoryte@gmail.com>
2022-05-25 18:26:02 +0300

    Added back button, no you dont need to click on browser back, same function does back button

--- index.php
+++ index.php
@@ -27,13 +27,16 @@
 
       #button {
          background-color: #097969;
-         width:50%;
-         padding:1rem;
-         color:white;
+         padding:0.5rem;
          border-radius:5px;
          display:block;
          margin:0 auto;
          border: none;
+      }
+      #button a{
+         text-decoration: none;
+         color:white;
+         padding:0.5rem 3rem 0.5rem 3rem;
       }
    </style>
 </head>
@@ -41,7 +44,7 @@
 <body>
    <?php
    print("<h1>Current Directory: " . $_SERVER['REQUEST_URI'] . "</h1>");
-   print("<h1> Current directory:" . str_replace("?path=", "", $_SERVER['REQUEST_URI']) . "</h1>");
+   print("<h1> Current directory:" . str_replace("?path=./", "", $_SERVER['REQUEST_URI']) . "</h1>");
    print("<table id='table'><tr><th>Type</th><th>Name</th><th>Action</th></tr>");
    if (isset($_GET["path"])) {
       $path = $_SERVER['REQUEST_URI'];
@@ -50,9 +53,6 @@
    }
    $dir = "./";
    $dirPath = $dir . $_GET["path"];
-   print($dirPath);
-   print("<br>");
-   print($_GET["path"]);
    $dirArr = scandir($dirPath);
    $dirArr = array_diff($dirArr, array(".", ".."));
    $dirArr = array_values($dirArr);
@@ -60,13 +60,15 @@
    foreach ($dirArr as $dirValue) {
       if (is_dir($dirPath . $dirValue)) {
          print("<tr><td>Folder</td><td><a href=" . $path . $dirValue . "/" . ">$dirValue</a></td><td></td></tr>");
-         // print("</tr>");
       } else if (is_file($dirPath . $dirValue)) {
          print("<tr><td>File</td><td>$dirValue</td><td></td></tr>");
       }
    }
    // print($_GET["path"]);
+  
    print("</table>");
+$backPath = "?path=".dirname($_GET["path"])."/";
+   print("<button id='button'><a href=".$backPath.">Back</a></button>");
    ?>
 </body>
 

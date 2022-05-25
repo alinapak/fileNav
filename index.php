40aede71174002e4db735e8aa1c4712054c3d253
alinapak <alina.pakamoryte@gmail.com>
2022-05-25 12:10:49 +0300

    fixed walking through directories get path mistake

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
    <style>
       #table {
          width: 100%;
+         margin-bottom:2rem;
       }
 
       #table td,
@@ -23,32 +24,49 @@
          background-color: #097969;
          color: white;
       }
+
+      #button {
+         background-color: #097969;
+         width:50%;
+         padding:1rem;
+         color:white;
+         border-radius:5px;
+         display:block;
+         margin:0 auto;
+         border: none;
+      }
    </style>
 </head>
 
 <body>
    <?php
-      print("<h1>Current Directory: " . $_SERVER['REQUEST_URI'] . "</h1>");
-      print("<h1> Current directory:" . str_replace("?path=", " ", $_SERVER['REQUEST_URI']) . "</h1>");
-      print("<table id='table'><tr><th>Type</th><th>Name</th><th>Action</th></tr>");
-      $dir = "./";
-      $dirPath = $dir . $_GET["path"];
-      $dirArr = scandir($dirPath);
-      $dirArr = array_diff($dirArr, array(".", ".."));
-      $dirArr = array_values($dirArr);
+   print("<h1>Current Directory: " . $_SERVER['REQUEST_URI'] . "</h1>");
+   print("<h1> Current directory:" . str_replace("?path=", "", $_SERVER['REQUEST_URI']) . "</h1>");
+   print("<table id='table'><tr><th>Type</th><th>Name</th><th>Action</th></tr>");
+   if (isset($_GET["path"])) {
+      $path = $_SERVER['REQUEST_URI'];
+   } else {
+      $path =  $_SERVER['REQUEST_URI'] . '?path=';
+   }
+   $dir = "./";
+   $dirPath = $dir . $_GET["path"];
+   print($dirPath);
+   print("<br>");
+   print($_GET["path"]);
+   $dirArr = scandir($dirPath);
+   $dirArr = array_diff($dirArr, array(".", ".."));
+   $dirArr = array_values($dirArr);
 
-      foreach ($dirArr as $dirValue) {
-         if (is_dir($dirPath.$dirValue)) {
-            $newPath =  $_SERVER['REQUEST_URI'] . '?path=' . $dirValue . "/";
-            print("<tr><td>Folder</td>");
-            print("<td><a href=" . $newPath . ">$dirValue</a></td></tr>");
-            // print("</tr>");
-         } else if (is_file($dirPath.$dirValue)) {
-            print("<tr><td>File</td><td>$dirValue</td></tr>");
-         }
+   foreach ($dirArr as $dirValue) {
+      if (is_dir($dirPath . $dirValue)) {
+         print("<tr><td>Folder</td><td><a href=" . $path . $dirValue . "/" . ">$dirValue</a></td><td></td></tr>");
+         // print("</tr>");
+      } else if (is_file($dirPath . $dirValue)) {
+         print("<tr><td>File</td><td>$dirValue</td><td></td></tr>");
       }
-      // print($_GET["path"]);
-      print("</table>");
+   }
+   // print($_GET["path"]);
+   print("</table>");
    ?>
 </body>
 
